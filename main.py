80f92e1eb718887457b37d085eac142235b7093c
Anwar Anarkulov <anwar.anarkulov.2302@gmail.com>
2022-09-19 10:12:11 +0500

    Ввод модели нейронной связи через csv

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from neuron import *
 if __name__ == '__main__':
     adam = Neuron(1, 3, 2, 2)
     adam.set_synapses("00 01 01")
+    counter = 0
+    vector = [True, True, True]
     print(adam.get_result([True, True, True]))
 
--- network.py
+++ network.py
@@ -1,1 +1,6 @@
-pass
+from neuron import *
+
+class Network:
+    def __init__(self, levels_amount):
+        self.levels_amount = levels_amount
+
