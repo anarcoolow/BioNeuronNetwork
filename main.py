2fc54a1d4b054330a2402857bb4aef8166aa3fe5
Anwar Anarkulov <anwar.anarkulov.2302@gmail.com>
2022-09-20 09:38:16 +0500

    подсчет количества выходных синапсов

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from network import *
 import pandas as pd
 
 if __name__ == '__main__':
-    adam = Neuron(1, 3, 2, 2)
-    adam.set_synapses("00 01 01")
-    counter = 0
-    vector = [True, True, True]
-    print(adam.get_result([True, True, True]))
+    # adam = Neuron(1, 3, 2, 2)
+    # adam.set_synapses("00 01 01")
+    # counter = 0
+    # vector = [True, True, True]
+    # print(adam.get_result([True, True, True]))
 
     df = pd.read_csv("model_input.csv")
     eden = Network(df)
-    print(eden)
+    print("Расположение и тип синапсов в сети:\n", eden, sep='')
+    print("Количество внешних синапсов:", eden.amount_of_external)
--- network.py
+++ network.py
@@ -8,17 +8,8 @@ class Network:
         self.levels_amount = len(df['level'].unique())  # кол-во элементов
         self.levels = []
         for i in range(self.levels_amount):
-            self.levels.append(self.get_neurons_of_this_level(df, i).copy())
-
-    def get_neurons_of_this_level(self, df: pd.DataFrame, number: int) -> List[Neuron]:
-        result = []
-        needed = df[df['level'].isin([number])]
-        needed = needed.to_dict('records')
-        for line in needed:
-            neuron = Neuron(line['id'], line['synapses'], line['threshold'], line['activators'])
-            neuron.set_synapses(line['connections'])
-            result.append(neuron)  # result[-1].set_synapses(line['connections'])
-        return result
+            self.levels.append(self._get_neurons_of_this_level(df, i).copy())
+        self.amount_of_external = self._get_amount_of_external_synapses()
 
     def __str__(self):
         result = ""
@@ -28,4 +19,22 @@ class Network:
                     result += str(int(synapse.type))
                 result += ' '
             result += '\n'
+        return result
+
+    def _get_amount_of_external_synapses(self):
+        result = 0
+        for level in self.levels:
+            for neuron in level:
+                for synapses in neuron.synapses:
+                    result += 1 if not synapses.source_id else result
+        return result
+
+    def _get_neurons_of_this_level(self, df: pd.DataFrame, number: int) -> List[Neuron]:
+        result = []
+        needed = df[df['level'].isin([number])]
+        needed = needed.to_dict('records')
+        for line in needed:
+            neuron = Neuron(line['id'], line['synapses'], line['threshold'], line['activators'])
+            neuron.set_synapses(line['connections'])
+            result.append(neuron)  # result[-1].set_synapses(line['connections'])
         return result
